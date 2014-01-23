bb0eba3cf2ade8cc1017ffb21abf5f362f50cca9
tknopp <tobias@knoppweb.de>
2014-01-23 01:27:46 +0100

    minor

--- src/JuliaTools.jl
+++ src/JuliaTools.jl
@@ -1,34 +1,35 @@
-module JuliaTools
+#module JuliaTools
 
-export pkgviewer
+#export pkgviewer
 
 using Gtk.ShortNames
 
 function pkgviewer()
-  builder = Builder(joinpath(dirname(Base.source_path()),"pkgviewer.xml"))
-  
+  builder = Builder("C:\\Users\\knopp\\.julia\\JuliaTools.jl\\src\\pkgviewer.xml")
+  #builder = Builder(joinpath(dirname(Base.source_path()),"pkgviewer.xml"))
+
   installedPkg = Pkg.installed()
   
   ls=ListStore(String,String)
   
   for (name,version) in installedPkg
-    push!(ls,(name,version))
+    push!(ls,(name,string(version)))
   end
   
   tv=TreeView(ls)
   r1=CellRendererText()
   c1=TreeViewColumn("Name", r1,text=0)
-  c2=TreeViewColumn("Version", r1,active=1)
+  c2=TreeViewColumn("Version", r1,text=1)
   push!(tv,c1)
   push!(tv,c2) 
   
   hbox1 = G_.object(builder,"hbox1")
-  hbox1[2] = tv
-  hbox1[tv,:expand]
+  push!(hbox1,tv)
+  hbox1[tv,:expand] = true
   
   win = G_.object(builder,"mainWindow")
-  showall(win)
+  show(win)
 end
 
 
-end #module
+#end #module
